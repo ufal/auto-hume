aff0195fddf65bdb8d2623c30840faaa8c6d5209
David Mareček <marecek@ufal.mff.cuni.cz>
2017-05-16 14:09:58 +0200

    parallel subtrees for computing chrF on them

---
+++ scripts/get_parallel_subtrees.pl
@@ -0,0 +1,95 @@
+#!/usr/bin/env perl
+
+binmode STDIN, ":utf8";
+binmode STDOUT, ":utf8";
+
+use strict;
+use warnings;
+use Getopt::Long;
+
+my $REF_TREES_FILENAME = undef;
+my $ALIGNMENT_FILENAME = undef;
+my $TRANSLATION_FILENAME = undef;
+
+GetOptions ("ref-trees=s" => \$REF_TREES_FILENAME,
+            "alignment=s" => \$ALIGNMENT_FILENAME,
+            "translation=s" => \$TRANSLATION_FILENAME
+           ) or die("Error in command line arguments\n");
+
+my @children;
+my @ref_words;
+my $sent_num = 0;
+
+open(CONLLU, "<:utf8", $REF_TREES_FILENAME) or die;
+while(<CONLLU>) {
+    chomp;
+    if ($_ =~ /^\d/) {
+        my @items = split(/\t/, $_);
+        push @{$children[$sent_num][$items[6]]}, $items[0];
+        $ref_words[$sent_num][$items[0]] = $items[1];
+    }
+    else {
+        $sent_num++;
+    }
+}
+close CONLLU;
+print STDERR "Trees sentences: $sent_num\n";
+
+sub get_descendants {
+    my ($s, $w) = @_;
+    my @descendants = ($w);
+    if (defined $children[$s][$w]) {
+        foreach my $c (@{$children[$s][$w]}) {
+            push @descendants, get_descendants($s, $c);
+        }
+    }
+    return @descendants;
+}
+
+$sent_num = 0;
+my @alignment;
+open(ALI, "<:utf8", $ALIGNMENT_FILENAME) or die;
+while(<ALI>) {
+    chomp;
+    my @pairs = split(/\s/, $_);
+    foreach my $pair (@pairs) {
+        my ($ref, $tgt) = split(/-/, $pair);
+        $alignment[$sent_num][$ref + 1] = $tgt + 1;
+    }
+    $sent_num++;
+}
+close ALI;
+print STDERR "Alignment sentences: $sent_num\n";
+
+$sent_num = 0;
+my @tgt_words;
+open(TRANSLATION, "<:utf8", $TRANSLATION_FILENAME) or die;
+while(<TRANSLATION>) {
+    chomp;
+    my @words = split(/\s/, $_);
+    unshift @words, "";
+    $tgt_words[$sent_num] = \@words;
+    $sent_num++;
+}
+print STDERR "Translation sentences: $sent_num\n";
+
+foreach my $s (0 .. $sent_num - 1) {
+    print STDERR "$#{$ref_words[$s]} ";
+    foreach my $w (0 .. $#{$ref_words[$s]}) {
+        my @tgt_w;
+        my @ref_w;
+        foreach my $d (get_descendants($s, $w)) {
+            if (defined $alignment[$s][$d]) {
+                push @ref_w, $d;
+                push @tgt_w, $alignment[$s][$d];
+            }
+        }
+        my @ref_subtree = map{$ref_words[$s][$_]} sort @ref_w;
+        my @tgt_subtree = map{$tgt_words[$s][$_]} sort @tgt_w;
+        print join(" ", @ref_subtree);
+        print "\t";
+        print join(" ", @tgt_subtree);
+        print "\n";
+    }
+    print "\n";
+}
