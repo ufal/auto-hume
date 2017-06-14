73533c4bcfd3c74fe3fdb046b23ef710a0f04250
rosa <rosa@ufal.mff.cuni.cz>
2017-06-14 02:57:08 +0200

    bugfix in copmputze_autohume

--- scripts/compute_autohume.pl
+++ scripts/compute_autohume.pl
@@ -10,20 +10,25 @@ my $current_sent = 0;
 my $sum_chrF = 0;
 my $count_chrF = 0;
 
+sub sent_end {
+
+    my $score = $sum_chrF / $count_chrF;
+    print "$score\n";
+    $sum_chrF = 0;
+    $count_chrF = 0;
+    $current_sent++;
+
+    return ;
+}
+
 while(<>) {
     chomp;
     my ($sent_num, $weight, $ref, $trans, $chrF) = split (/\t/, $_);
-    if ($sent_num == $current_sent) {
-        $sum_chrF += $weight * $chrF;
-        $count_chrF += $weight;
+    if ($sent_num != $current_sent) {
+        sent_end();
     }
-    else {
-        my $score = $sum_chrF / $count_chrF;
-        print "$score\n";
-        $sum_chrF = 0;
-        $count_chrF = 0;
-        $current_sent = $sent_num;
-    }
+    $sum_chrF += $weight * $chrF;
+    $count_chrF += $weight;
 }
-my $score = $sum_chrF / $count_chrF;
-print "$score\n";
+sent_end();
+
